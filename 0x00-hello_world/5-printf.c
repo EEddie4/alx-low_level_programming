ba128f687596430e0c65c6551ce68561f1931f05
EEddie4 <eddiejgamer4@gmail.com>
2022-09-08 16:41:14 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  The entry point of this program
+ *
+ * Description: Print out a message to the user.
+ *
+ * Return: 0 everytime
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
