f5fb25520e4e566a2d0e29519cd00d20d9dbfa2f
EEddie4 <eddiejgamer4@gmail.com>
2022-09-08 16:56:20 -0700

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point, all ways na ways.
+ * Description: Prints a weird quote.
+ * Return: 0 everytime
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - D\
+ora Korpar, 2015-10-19\n");
+	return (1);
+}
