5fc2abf86edd91c213c1c3c97e5f2dc8be468c51
EEddie4 <eddiejgamer4@gmail.com>
2022-09-13 01:06:36 -0700

    A

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/*
+ * main: Starts execution of code
+ * Description: Prints a string
+ * Return: returns 0
+ */
+
+int main (void)
+
+{
+	printf("_putchar\n");
+	return (0);
+}
