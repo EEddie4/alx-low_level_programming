20856a9dcb9acc6e1066d35ed8ce5bea7f2c9cb7
EEddie4 <eddiejgamer4@gmail.com>
2022-09-15 16:33:10 -0700

    A

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,8 +1,9 @@
 #include"main.h"
-/**
+
+/*
  * print_diagonal - print a diagonal line
  * @n: is the number of times the \ character
- * should be printed
+ *  should be printed
  */
 
 void print_diagonal(int n)
