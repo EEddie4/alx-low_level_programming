d8af40217a8aeef009a7d35b076bc0081e3fbc06
EEddie4 <eddiejgamer4@gmail.com>
2022-09-15 17:34:50 -0700

    A

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,9 +13,11 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
+
 	/*print the first few digits*/
 	if ((num / 10) > 0)
 		print_number(num / 10);
+
 	/*print the last digit*/
 	_putchar((num % 10) + 48);
 }
