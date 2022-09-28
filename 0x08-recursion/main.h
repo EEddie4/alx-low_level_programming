b89034887943ff64b543b7a30494ff936a421471
EEddie4 <eddiejgamer4@gmail.com>
2022-09-28 13:24:09 -0700

    A

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,3 +5,14 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _is_prime_number(int n);
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+/**
+ * *void prototypes(void)
+ * *int prototypes(int)
+ * *char prototypes(char) 
+ * */
+int is_prime_number(int n);
+int is_prime(int n, int start);
+#endif /* MAIN_H */
