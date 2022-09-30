de7a7de818b6f38361d814b7d294cbf75a3049a6
EEddie4 <eddiejgamer4@gmail.com>
2022-09-30 14:06:55 -0700

    A

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints the minimum number of coins to make change for an amount.
+ * main-prints the minimum number of coins to make change for an amount.
  * of money.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
@@ -16,9 +16,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	cents = atoi(argv[1]);
-
 	while (cents > 0)
 	{
 		if (cents >= 25)
